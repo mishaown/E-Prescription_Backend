8946c7b5b5b0a7352ea68c61dd8562228c6cfbe8
sh40wn <sh4wn@hotmail.com>
2021-12-02 18:08:50 +0600

    checkpoint-0

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    
-    const conn = await mongoose.connect(process.env.MONGODB_URI_DATA,
-        {
-            useCreateIndex: true,
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        });
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI_DATA, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.inverse);
-}
+  } catch (error) {
+    console.log(`MongoDB Error: ${error.message}`.red.inverse);
+  }
+};
 
 module.exports = connectDB;
